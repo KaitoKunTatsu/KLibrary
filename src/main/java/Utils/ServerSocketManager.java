02e03d2d3765dbd7ec7fe1efd1c77c57e39b1f6c
KaitoKunTatsu <kaitokuntatsu@gmail.com>
2022-08-12 17:12:30 +0200

    JavaIO server socket manager

--- src/main/java/Utils/ServerSocketManager.java
+++ src/main/java/Utils/ServerSocketManager.java
@@ -1,5 +1,7 @@
 package Utils;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -9,15 +11,51 @@ import java.util.List;
 public class ServerSocketManager {
 
     private final ServerSocket serverSocket;
-    private final List<Socket> clientSockets;
+    // [ [Socket, OutStream, InStream], [...] ]
+    private final List<List<Object>> clientSockets;
 
-    public ServerSocketManager(int pPort) throws IOException {
+    public ServerSocketManager(int pPort, int pIOTimeout) throws IOException {
         serverSocket = new ServerSocket(pPort);
         clientSockets = new ArrayList<>();
+        serverSocket.setSoTimeout(pIOTimeout);
     }
 
-    public void close() {
-        for (Socket socket : clientSockets){
+    public void acceptSocket() throws IOException {
+        Socket newSocket = serverSocket.accept();
+        DataOutputStream outStream = new DataOutputStream(newSocket.getOutputStream());
+        DataInputStream inStream = new DataInputStream(newSocket.getInputStream());
+        clientSockets.add(new ArrayList<>() {{ add(newSocket); add(outStream); add(inStream); }});
+    }
+
+    public Socket getClientSocket(int pIndex)
+    {
+        return (Socket) clientSockets.get(pIndex).get(0);
+    }
+
+    public String readInputFromSocket(int pIndex) {
+        try {
+            return ((DataInputStream)clientSockets.get(pIndex).get(2)).readUTF();
         }
+        catch (IOException e) {
+            return "";
+        }
+    }
+
+    public void writeToSocket(String pMessage, int pIndex) throws IOException {
+        ((DataOutputStream) clientSockets.get(pIndex).get(1)).writeUTF(pMessage);
+    }
+
+    public void close() throws IOException {
+        for (int i = 0; i < clientSockets.size(); i++)
+        {
+            try {
+                ((Socket)clientSockets.get(i).get(0)).close();
+                ((DataOutputStream)clientSockets.get(i).get(1)).close();
+                ((DataInputStream)clientSockets.get(i).get(2)).close();
+            }
+            catch (IOException ignored) {}
+            finally {clientSockets.remove(i);}
+        }
+        serverSocket.close();
     }
 }
