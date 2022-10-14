e23c8ca86a278ca120f88f71046acb041e261a5c
KaitoKunTatsu <kaitokuntatsu@gmail.com>
2022-10-14 09:05:08 +0200

    clean up and forgot optional password

--- src/main/java/KLibrary/utils/SQLUtils.java
+++ src/main/java/KLibrary/utils/SQLUtils.java
@@ -6,13 +6,12 @@ import java.sql.*;
  * This class provides methods for secure SQL statements (preventing SQL-injection)<br>
  * A part of the (<a href="https://github.com/KaitoKunTatsu/KLibrary">KLibrary</a>)
  *
- * @version 1.1.3 | last edit: 26.09.2022
+ * @version 3.0.2 | last edit: 14.10.2022
  * @author Joshua H. | KaitoKunTatsu#3656
  */
 public class SQLUtils {
 
     private Connection con;
-    private PreparedStatement stmt;
 
     /**
      * Creates an instance of this class and establishes a database connection.
@@ -25,30 +24,41 @@ public class SQLUtils {
         setDatabase(pDBPath);
     }
 
+    public SQLUtils(String pDatabaseUrl, String pUser, String pPassword) throws SQLException {
+        setDatabase(pDatabaseUrl, pUser, pPassword);
+    }
+
+    private PreparedStatement addStatementVariables(String pStatement, Object... pSet) throws SQLException
+    {
+        PreparedStatement lStmt = con.prepareStatement(pStatement);
+        addStatementVariables(lStmt,pSet);
+        return lStmt;
+    }
+
+    private void addStatementVariables(PreparedStatement pStatement, Object... pSet) throws SQLException
+    {
+        if (pSet == null) return;
+        for (int i = 0; i < pSet.length; i++)
+        {
+            if (Blob.class.equals(pSet[i].getClass())) pStatement.setBlob(i + 1, (Blob) pSet[i]);
+            else if (byte[].class.equals(pSet[i].getClass())) pStatement.setBytes(i + 1, (byte[]) pSet[i]);
+            else if (byte.class.equals(pSet[i].getClass())) pStatement.setByte(i + 1, (byte) pSet[i]);
+            else if (String.class.equals(pSet[i].getClass())) pStatement.setString(i + 1, (String) pSet[i]);
+            else if (Integer.class.equals(pSet[i].getClass())) pStatement.setInt(i + 1, (int) pSet[i]);
+            else if (Long.class.equals(pSet[i].getClass())) pStatement.setLong(i + 1, (long) pSet[i]);
+            else if (Boolean.class.equals(pSet[i].getClass())) pStatement.setBoolean(i + 1, (boolean) pSet[i]);
+            else if (Double.class.equals(pSet[i].getClass())) pStatement.setDouble(i + 1, (double) pSet[i]);
+            else if (Date.class.equals(pSet[i].getClass())) pStatement.setDate(i + 1, (Date) pSet[i]);
+        }
+    }
+
     /**
      * @param pStatement SQL statement
      * @param pSet Each ? in the statment will be replaced with the content of this array
      * @return a {@link ResultSet} containing the result of your SQL statement
      * */
     public ResultSet onQuery(String pStatement, Object... pSet) throws SQLException {
-        stmt = con.prepareStatement(pStatement);
-        if (pSet != null)
-        {
-            for (int i = 0; i < pSet.length; i++) {
-                if (Blob.class.equals(pSet[i].getClass())) stmt.setBlob(i + 1, (Blob) pSet[i]);
-                else if (byte[].class.equals(pSet[i].getClass())) stmt.setBytes(i + 1, (byte[]) pSet[i]);
-                else if (byte.class.equals(pSet[i].getClass())) stmt.setByte(i + 1, (byte) pSet[i]);
-                else if (String.class.equals(pSet[i].getClass())) stmt.setString(i + 1, (String) pSet[i]);
-                else if (Integer.class.equals(pSet[i].getClass())) stmt.setInt(i + 1, (int) pSet[i]);
-                else if (Long.class.equals(pSet[i].getClass())) stmt.setLong(i + 1, (long) pSet[i]);
-                else if (Boolean.class.equals(pSet[i].getClass())) stmt.setBoolean(i + 1, (boolean) pSet[i]);
-                else if (Double.class.equals(pSet[i].getClass())) stmt.setDouble(i + 1, (double) pSet[i]);
-                else if (Date.class.equals(pSet[i].getClass())) stmt.setDate(i + 1, (Date) pSet[i]);
-            }
-        }
-        ResultSet rs = stmt.executeQuery();
-        stmt.clearParameters();
-        return rs;
+        return addStatementVariables(pStatement, pSet).executeQuery();
     }
 
     /**
@@ -64,25 +74,8 @@ public class SQLUtils {
      * @param pSet Each ? in the statment will be replaced with the content of this array
      * */
     public void onExecute(String pStatement, Object... pSet) throws SQLException {
-        stmt = con.prepareStatement(pStatement);
-        if (pSet != null)
-        {
-            for (int i=0; i < pSet.length; i++)
-            {
-                if (Blob.class.equals(pSet[i].getClass())) stmt.setBlob(i + 1, (Blob) pSet[i]);
-                else if (byte[].class.equals(pSet[i].getClass())) stmt.setBytes(i + 1, (byte[]) pSet[i]);
-                else if (byte.class.equals(pSet[i].getClass())) stmt.setByte(i + 1, (byte) pSet[i]);
-                else if (String.class.equals(pSet[i].getClass())) stmt.setString(i + 1, (String) pSet[i]);
-                else if (Integer.class.equals(pSet[i].getClass())) stmt.setInt(i + 1, (int) pSet[i]);
-                else if (Long.class.equals(pSet[i].getClass())) stmt.setLong(i + 1, (long) pSet[i]);
-                else if (Boolean.class.equals(pSet[i].getClass())) stmt.setBoolean(i + 1, (boolean) pSet[i]);
-                else if (Double.class.equals(pSet[i].getClass())) stmt.setDouble(i + 1, (double) pSet[i]);
-                else if (Date.class.equals(pSet[i].getClass())) stmt.setDate(i + 1, (Date) pSet[i]);
-            }
-        }
-        stmt.execute();
+        addStatementVariables(pStatement, pSet).execute();
         con.commit();
-        stmt.clearParameters();
     }
 
     /**
@@ -100,6 +93,11 @@ public class SQLUtils {
      * */
     public void setDatabase(String pDBPath) throws SQLException {
         con = DriverManager.getConnection("jdbc:sqlite:"+pDBPath);
+        con.setAutoCommit(false);
+    }
+
+    public void setDatabase(String pDatabaseUrl, String pUser, String pPassword) throws SQLException {
+        con = DriverManager.getConnection("jdbc:sqlite:"+pDatabaseUrl, pUser, pPassword);
         con.setAutoCommit(false);
     }
 
